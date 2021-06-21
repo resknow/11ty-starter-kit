663d8d96035fd82f47d16c6b3e4a9b81a219223e
Chris Galbraith <chris@resknow.co.uk>
2021-06-21 16:26:42 +0100

    Add esbuild

---
+++ templates/_helpers/bundle.11ty.js
@@ -0,0 +1,38 @@
+/**
+ * Adapted from Adam K Dean
+ * @see https://dev.to/adamkdean/simple-scss-with-11ty-kmn
+ */
+
+const esbuild = require('esbuild');
+const globby = require('globby');
+const siteConfig = require('../_data/site');
+
+module.exports = class {
+	data() {
+		return {
+			layout: false,
+			permalink: '/bundle.js',
+			eleventyExcludeFromCollections: true
+		};
+	}
+
+	async render() {
+		let inputFiles = await globby('./assets/js/bundle/*.js');
+		if (inputFiles.length === 0) return;
+
+		let output = [];
+
+		let result = esbuild.buildSync({
+			minify: true,
+			entryPoints: inputFiles,
+			write: false,
+			outdir: '<stdout>'
+		});
+
+		for (let out of result.outputFiles) {
+			output.push(new TextDecoder('utf-8').decode(out.contents));
+		}
+
+		return output.join('\n');
+	}
+};
